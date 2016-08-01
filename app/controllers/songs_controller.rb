cbcc20adf19100eda3b2ab9a14b52effbfd9895b
Gordon Gao <gordonsdev@gmail.com>
2016-08-01 15:22:40 +0000

    Done.

--- app/controllers/songs_controller.rb
+++ app/controllers/songs_controller.rb
@@ -1,10 +1,30 @@
 class SongsController < ApplicationController
   def index
-    @songs = Song.all
+    if params[:artist_id]
+      artist = Artist.find(params[:artist_id]) rescue nil
+      if artist
+        @songs = artist.songs
+      else
+        flash[:alert] = "Artist not found"
+        redirect_to artists_path
+      end
+    else
+      @songs = Song.all
+    end
   end
 
   def show
-    @song = Song.find(params[:id])
+    if params[:artist_id]
+      @song = Song.find(params[:id]) rescue nil
+
+      unless @song
+        flash[:alert] = "Song not found"
+        redirect_to artist_songs_path(params[:artist_id])
+      end
+    else
+      @song = Song.find(params[:id])
+    end
+
   end
 
   def new
@@ -50,4 +70,3 @@ class SongsController < ApplicationController
     params.require(:song).permit(:title, :artist_name)
   end
 end
-
