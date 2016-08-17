c53308250193af6e5931784ba73c3c68b01f3812
irevived1 <haifeng.wu@jjay.cuny.edu>
2016-08-17 16:45:40 -0400

    Done.

--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,10 @@
 Rails.application.routes.draw do
-  resources :artists
+  # resources :artists
+
+  # resources :artists
+  # resources :artist , only: [:show,:index] do
+  resources :artists do
+    resources :songs, only: [:show,:index]
+  end
   resources :songs
 end
