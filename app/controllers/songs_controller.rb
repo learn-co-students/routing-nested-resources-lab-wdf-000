2305fbc953e668f226afd05fe7e91d5aa35fb959
fidelscodes <fidelscodes@gmail.com>
2016-09-23 16:36:13 -0400

    update songs controller pass all tests

--- app/controllers/songs_controller.rb
+++ app/controllers/songs_controller.rb
@@ -1,9 +1,15 @@
 class SongsController < ApplicationController
   def index
-    if !params[:artist_id] 
-      @songs = Song.all
+    if params[:artist_id] 
+      @artist = Artist.find_by(id: params[:artist_id])
+      if @artist
+        @songs = @artist.songs
+      else
+        flash[:notice] = 'Artist not found.'
+        redirect_to artists_path
+      end
     else
-      redirect_to artists_path
+      @songs = Song.all
     end
   end
 
