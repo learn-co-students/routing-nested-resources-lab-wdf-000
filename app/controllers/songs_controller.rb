db922c0305207780f872abb2822a8184386cebb3
Natalisp <nathalipat@gmail.com>
2016-09-26 14:10:46 -0400

    Done.

--- app/controllers/songs_controller.rb
+++ app/controllers/songs_controller.rb
@@ -1,10 +1,29 @@
 class SongsController < ApplicationController
+require 'pry'
   def index
-    @songs = Song.all
+    if params[:artist_id]
+       @artist = Artist.find_by(id: params[:artist_id])
+        if @artist.nil?
+           redirect_to artists_path, alert: 'Artist not found.'
+        else
+           @songs = @artist.songs
+        end
+    else
+       @songs = Song.all
+   end
   end
 
   def show
-    @song = Song.find(params[:id])
+    if params[:artist_id]
+     @artist = Artist.find_by(id: params[:artist_id])
+     # binding.pry
+     @song = @artist.songs.find_by(id: params[:id])
+       if @song.nil?
+          redirect_to artist_songs_path, alert: 'Song not found.'
+      end
+    else
+     @song = Song.find(params[:id])
+   end
   end
 
   def new
